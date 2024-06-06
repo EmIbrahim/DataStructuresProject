349971f19db7b4d5d5dd7edf0799431ea2b7933c
Muhammad Ibrahim <113703307+EmIbrahim@users.noreply.github.com>
2024-06-06 19:41:02 +0500

    Create Pair.java

---
+++ code/Pair.java
@@ -0,0 +1,24 @@
+package music;
+
+public class Pair<K extends Comparable<K>, V> implements Comparable<Pair<K, V>> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(Pair<K, V> other) {
+        return this.key.compareTo(other.getKey());
+    }
+}
